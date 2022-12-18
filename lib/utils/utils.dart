37f4ee42b84c6aa16e5dbb06373ea126f406330d
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-12-18 19:56:09 +0200

    forgetSemiColon_AbdulrahmanAmir

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,5 +4,5 @@ import 'package:flutter/material.dart';
 void showSnakBar(BuildContext context, String content){
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(content: Text(content))
-  )
+  );
 }
