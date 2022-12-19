283cb79fbbdecb1e94fa3074dbc7a00f37ef2fd3
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-12-19 17:45:47 +0200

    VerifiyingOTP & adding DATABASE Operations

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase/Screens/OTP_Screen.dart';
 import 'package:firebase/utils/utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,8 +10,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AuthProvider extends ChangeNotifier {
   bool _isSignedIn = false;
   bool get isSignedIn => _isSignedIn;
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+  String? _uid;
+  String get uid => _uid!;
 
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
 
   AuthProvider() {
     checkSgin();
@@ -22,6 +28,7 @@ class AuthProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+//sign in
   void signInWithPhone(BuildContext context, String phoneNumber) async {
     try {
       await _firebaseAuth.verifyPhoneNumber(
@@ -43,6 +50,48 @@ class AuthProvider extends ChangeNotifier {
           codeAutoRetrievalTimeout: ((verificationId) {}));
     } on FirebaseAuthException catch (e) {
       showSnakBar(context, e.message.toString());
+    }
+  }
+
+// verify otp
+  void verifyOtp({
+    required BuildContext context,
+    required String verificationId,
+    required String userOtp,
+    required Function onSuccess,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      PhoneAuthCredential creds = PhoneAuthProvider.credential(
+          verificationId: verificationId, smsCode: userOtp);
+      //user
+      User? user = (await _firebaseAuth.signInWithCredential(creds)).user!;
+      if (user != null) {
+        //carry our logic
+        _uid = user.uid;
+        onSuccess();
+      }
+      _isLoading = false;
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      showSnakBar(context, e.message.toString());
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+// DATANASE OPERATIONS
+  Future<bool> checkExistingUser() async {
+    DocumentSnapshot snapshot =
+        await _firebaseFirestore.collection("users").doc(_uid).get();
+    if (snapshot.exists) {
+      print("USER EXISTS");
+      return true;
+    } else {
+      print("NEW USER");
+      return false;
     }
   }
 }
